eb74546cf2e1f9b53bcf266f6542c0606f101f79
controlrampa2h8-cmd <controlrampa2h8@gmail.com>
2026-06-29 15:29:24 -0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 // REEMPLAZA ESTOS VALORES CON LOS DE TU PROYECTO SUPABASE
 const SUPABASE_URL = 'https://xgqvlvuuyltqolwkcqom.supabase.co/rest/v1/';
-const SUPABASE_KEY = 'sb_publishable_pIophqrAz1D1n1aQU2_Jzg_hsZy6IJD';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhncXZsdnV1eWx0cW9sd2tjcW9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3NTU1NzUsImV4cCI6MjA5ODMzMTU3NX0.WrC_E5PY1PDq9FXMxekrONdC-DqjzHws7sz_CwZASjk';
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
