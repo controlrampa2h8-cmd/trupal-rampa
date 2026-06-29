3bd722b110633f7261808830b4ec7f9dc1959927
controlrampa2h8-cmd <controlrampa2h8@gmail.com>
2026-06-29 15:23:03 -0500

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,5 @@
+// REEMPLAZA ESTOS VALORES CON LOS DE TU PROYECTO SUPABASE
+const SUPABASE_URL = 'https://xgqvlvuuyltqolwkcqom.supabase.co/rest/v1/';
+const SUPABASE_KEY = 'sb_publishable_pIophqrAz1D1n1aQU2_Jzg_hsZy6IJD';
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
